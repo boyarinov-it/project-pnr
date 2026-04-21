949d8d1ef9ae5e1ae801749ecd231aad0fc402f1
boyarinov-it <boyarinovet@gmail.com>
2026-04-21 18:10:22 +0300

    Add FastAPI config and environment settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "Project PNR API"
+    app_env: str = "local"
+    backend_port: int = 8000
+
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings()
