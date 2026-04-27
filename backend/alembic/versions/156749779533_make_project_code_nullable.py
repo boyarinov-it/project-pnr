6df9122722d1aec0e72bf6f274c04c26ebb735a6
boyarinov-it <boyarinovet@gmail.com>
2026-04-27 13:27:21 +0300

    fix: allow creating projects from frontend

---
+++ backend/alembic/versions/156749779533_make_project_code_nullable.py
@@ -0,0 +1,18 @@
+﻿"""make project code nullable"""
+
+from alembic import op
+
+
+revision = "156749779533"
+down_revision = "0416501fdcc8"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER TABLE projects ALTER COLUMN code DROP NOT NULL")
+
+
+def downgrade() -> None:
+    op.execute("UPDATE projects SET code = name WHERE code IS NULL")
+    op.execute("ALTER TABLE projects ALTER COLUMN code SET NOT NULL")
